daabd47b26a52ee04ce8e63044d65b881f37019d
Piotr Justyna <piotr.justyna@gmail.com>
2023-05-19 23:07:51 +0100

    First attempts at scnanning project files.

--- UnchartedDependencies/Program.fs
+++ UnchartedDependencies/Program.fs
@@ -1,2 +1,14 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+open System.Xml
+
+let xml = File.ReadAllText("./ScannedCode/SosNet.Cache.Lookup.fsproj")
+
+let doc = new XmlDocument() in doc.LoadXml xml;
+let referencesFound =
+    doc.SelectNodes "/Project/ItemGroup/ProjectReference/@Include"
+    |> Seq.cast<XmlNode>
+    |> Seq.map (fun node -> node.Value)
+    |> String.concat Environment.NewLine
+
+printfn "%s" referencesFound
