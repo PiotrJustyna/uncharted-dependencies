a9dca39316fa448705449135c5188ddcb34d1ec4
Piotr Justyna <piotr.justyna@gmail.com>
2023-05-22 16:13:05 +0100

    Mapping out sln -> projects.

--- UnchartedDependencies/Program.fs
+++ UnchartedDependencies/Program.fs
@@ -1,6 +1,12 @@
 ﻿open System
 open System.IO
+open System.Text.RegularExpressions
 open System.Xml
+
+let buildDictionary (input: string seq) : Collections.Generic.IDictionary<string, string> =
+    input
+    |> Seq.map (fun x -> Guid.NewGuid().ToString().Replace("-", ""), x)
+    |> dict
 
 let fileName = "./ScannedCode/SosNet.Cache.Lookup.fsproj"
 
@@ -23,21 +29,37 @@ printfn "---"
 printfn "title: fcc dependencies"
 printfn "---"
 printfn "stateDiagram-v2"
+printfn "direction lr"
 
-let files =
+let solutions =
     Directory.GetFiles("./ScannedCode/", "*.sln")
-    |> Seq.map (fun x -> Guid.NewGuid().ToString().Replace("-", ""), x)
+    |> Seq.map (fun x ->
+        let solution = File.ReadAllText x
+        let projects =
+            Regex.Matches(solution, "[a-zA-Z\\\.]+\.[c|f]{1}sproj")
+            |> Seq.map(fun y -> y.Value)
+            |> buildDictionary
+        Guid.NewGuid().ToString().Replace("-", ""), (x, projects))
     |> dict
 
 let states =
-    files
-    |> Seq.map (fun x -> $"state \"{x.Value}\" as {x.Key}")
+    solutions
+    |> Seq.map (fun x ->
+        let projectStates =
+            snd x.Value
+            |> Seq.map(fun y -> $"state \"{y.Value}\" as {y.Key}")
+            |> String.concat Environment.NewLine
+        $"state \"{fst x.Value}\" as {x.Key}{Environment.NewLine}{projectStates}")
     |> String.concat Environment.NewLine
 
 let connections =
-    files
+    solutions
     |> Seq.map (fun x ->
-        $"[*] --> {x.Key}{Environment.NewLine}{x.Key} --> [*]")
+        let projectSt =
+            snd x.Value
+            |> Seq.map (fun y -> $"{x.Key} --> {y.Key}")
+            |> String.concat Environment.NewLine
+        $"[*] --> {x.Key}{Environment.NewLine}{projectSt}")
     |> String.concat Environment.NewLine
 
 printfn $"{states}"
